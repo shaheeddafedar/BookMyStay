096553f55b670d47725256b841abfbf402f72176
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-06-12 23:29:30 +0530

    listing at frontend

--- frontend/src/context/listingContext.jsx
+++ frontend/src/context/listingContext.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useContext } from 'react'
 import { useState } from 'react'
 import { Children } from 'react'
 import { createContext } from 'react'
+import { authDataContext } from './Authcontext'
 
 export const listingDataContext = createContext()
 
 
 export default function listingContext({Children}) {
+let {serverUrl} = useContext(authDataContext)
+
+
 let [title, setTitle] = useState("");
 let [description, setDescription] = useState("");
 let [rent, setRent] = useState("");
@@ -22,9 +27,53 @@ let [backendimage1, backendsetImage1] = useState(null);
 let [backendimage2, backendsetImage2] = useState(null);
 let [backendimage3, backendsetImage3] = useState(null);
 
-  let value={
 
-  }
+
+const handleaddListing = async()=>{
+    try{
+const formData = new FormData();
+
+formData.append("title", title);
+formData.append("description", description);
+formData.append("rent", rent);
+formData.append("city", city);
+formData.append("landMark", landMark);
+formData.append("category", category);
+
+formData.append("image1", backendimage1);
+formData.append("image2", backendimage2);
+formData.append("image3", backendimage3);
+    
+    const result = await axios.post(serverUrl+"/api/listing/add",formData,{withCredentials:true})
+     console.log(result);
+    } catch(error){
+     console.log(error);
+     
+    }
+}
+
+let value = {
+  title,setTitle,
+
+  description,setDescription,
+
+  rent,setRent,
+
+  city,setCity,
+
+  landMark,setLandMark,
+
+  category,setCategory,
+
+ frontendimage1, frontendsetImage1,
+frontendimage2, frontendsetImage2,
+frontendimage3, frontendsetImage3,
+
+backendimage1, backendsetImage1,
+backendimage2, backendsetImage2,
+backendimage3, backendsetImage3
+
+};
 
   return (
     <div>
