96faa20c487ab5cbac526bc0e540cd5096c280f9
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-06-28 23:42:29 +0530

    adding up new controllet for updating or editing

--- backend/controllers/listingcontroller.js
+++ backend/controllers/listingcontroller.js
@@ -43,18 +43,18 @@ export const addListing = async (req, res) => {
         message: "User not found",
       });
     }
-    res.status(201).json(listing);
+   return res.status(201).json(listing);
   } catch (error) {
-    res.status(500).json({ message: `Adlisting error ${error}` });
+   return res.status(500).json({ message: `Adlisting error ${error}` });
   }
 };
 
 export const getListing = async (req, res) => {
   try {
     let listing = await Listing.find().sort({ createdAt: -1 });
-    res.status(200).json(listing);
+   return res.status(200).json(listing);
   } catch (error) {
-    res.status(500).json({ message: `getListing error ${error}` });
+   return res.status(500).json({ message: `getListing error ${error}` });
   }
 };
 
@@ -65,8 +65,56 @@ export const findListing = async (req, res) => {
     if (!listing) {
       res.status(404).json({ message: "listing not found" });
     }
-    res.status(200).json(listing);
+   return res.status(200).json(listing);
   } catch (error) {
-    res.status(500).json({ message: `FindingListing error ${error} `});
+   return res.status(500).json({ message: `FindingListing error ${error} `});
   }
 };
+
+
+export const updateListing = async (req,res) => {
+    try {
+    let {id} = req.params;
+    let { title, description, rent, city, landMark, category } = req.body;
+    let image1 = "";
+    let image2 = "";
+    let image3 = "";
+
+    if (req.files?.image1) {
+      image1 = await uploadOnCloudinary(req.files.image1[0].path);
+    }
+
+    if (req.files?.image2) {
+      image2 = await uploadOnCloudinary(req.files.image2[0].path);
+    }
+
+    if (req.files?.image3) {
+      image3 = await uploadOnCloudinary(req.files.image3[0].path);
+    }
+    let listing = await Listing.findByIdAndUpdate(id,{
+      title,
+      description,
+      rent,
+      city,
+      landMark,
+      category,
+      image1,
+      image2,
+      image3,
+      host,
+    },{new:true});
+    let user = await User.findByIdAndUpdate(
+      host,
+      { $push: { listing: listing._id } },
+      { returnDocument: "after" },
+    );
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    return res.status(201).json(listing);
+  } catch (error) {
+    return res.status(500).json({ message: `Adlisting error ${error}` });
+  }
+}
