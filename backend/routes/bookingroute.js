11fc2fd85b4372a83fcccc20c9c822f384c5bb13
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-04 00:29:48 +0530

    Add MyBooking feature

--- backend/routes/bookingroute.js
+++ backend/routes/bookingroute.js
@@ -2,11 +2,11 @@ import express from "express"
 
 
 import isAuth from "../middleware/isauth.js"
-import { createBooking } from "../controllers/bookingcontroller.js"
+import { cancelBooking, createBooking } from "../controllers/bookingcontroller.js"
 
 let bookingRouter = express.Router()
 
 bookingRouter.post("/create/:id",isAuth,createBooking)
-bookingRouter.delete("/create/:id",isAuth,createBooking)
+bookingRouter.delete("/cancel/:id",isAuth,cancelBooking)
 
 export default bookingRouter
